41d40c7a205d8e702872a123910cdd99c178f366
bhoopendra.modanwal <bhoopendra.modanwal@trigyn.com>
2024-08-08 13:42:05 +0530

    some more console log for finding correct Place

--- org.ekstep.metadata-1.5/editor/controller.js
+++ org.ekstep.metadata-1.5/editor/controller.js
@@ -162,9 +162,9 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
                 });
             })
         }  
-        console.log("associationsplugin before",associations);
+        console.log("getParentAssociationsplugin before",associations);
         associations.sort((a,b) => a.code.localeCompare(b.code, undefined, {numeric: true}));
-        console.log("associationsplugin after",associations);
+        console.log("getParentAssociationsplugin after",associations);
         callback(associations);
     };
 
@@ -206,6 +206,9 @@ angular.module('org.ekstep.metadataform', []).controller('metadataForm', ['$scop
                 })
             }
         });
+        console.log("getAssociations pulgin before==",associations);
+        associations.sort((a,b) => a.code.localeCompare(b.code, undefined, {numeric: true}));
+        console.log("getAssociations pulgin after==",associations);
         callback && callback(associations);
     }
 
